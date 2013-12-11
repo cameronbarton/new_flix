c2ae1217b0d8bede2a39ec5ed209a5b6ad31d450
Cameron Barton <cameront21@gmail.com>
2013-12-11 14:06:43 -0800

    Initialze repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewFlix::Application.config.secret_key_base = '77eb4ffe9c341db09c33bec206332579dbfe7c8694cadda4aa1881ae6c30f1c29daac336812f10a2649f2c4e8205dc132f62b5b05c42883ac2dac61a077e385f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewFlix::Application.config.session_store :cookie_store, key: '_new_flix_session'
