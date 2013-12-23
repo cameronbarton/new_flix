75c4175fee8079dc0d7a67b6e8c45a64c92d808b
Cameron Barton <cameront21@gmail.com>
2013-12-23 10:44:22 -0800

    Implement edit form to update and submit movies

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ NewFlix::Application.routes.draw do
   get "movies" => "movies#index"
 
   get "movies/:id" => "movies#show", as: "movie"
+
+  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+
+  patch "movies/:id" => "movies#update"
 end
