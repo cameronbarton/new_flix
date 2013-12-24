37ab1bb4c87e239c6d931029c56388e18b2e81cd
Cameron Barton <cameront21@gmail.com>
2013-12-23 16:30:39 -0800

    Implement create form to add new Movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 NewFlix::Application.routes.draw do
-  get "movies" => "movies#index"
-
-  get "movies/:id" => "movies#show", as: "movie"
-
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-
-  patch "movies/:id" => "movies#update"
+  root "movies#index"
+  resources :movies
 end
